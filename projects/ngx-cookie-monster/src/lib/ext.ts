3b3387f3c06835844f51bdefa35df1778b24f1c5
MI1490 <>
2019-09-18 12:37:16 +0200

    added new extensions
    - check if empty
    - check if is string
    - check if is number
    - decode URI
    - safe json parse

--- projects/ngx-cookie-monster/src/lib/ext.ts
+++ projects/ngx-cookie-monster/src/lib/ext.ts
@@ -1,7 +1,19 @@
 import {CookieOptions} from './cookie-options.model';
 
+export function empty(item: any): boolean {
+  return item === null && item === undefined;
+}
+
 export function exists(item: any): boolean {
   return item !== null && item !== undefined;
+}
+
+export function isString(item: any): boolean {
+  return typeof item === 'string';
+}
+
+export function isNumber(item: any): boolean {
+  return typeof item === 'number';
 }
 
 export function merge(options: CookieOptions, newOptions?: CookieOptions): CookieOptions {
@@ -14,7 +26,23 @@ export function merge(options: CookieOptions, newOptions?: CookieOptions): Cooki
     expires: exists(newOptions.expires) ? newOptions.expires : options.expires,
     secure: exists(newOptions.secure) ? newOptions.secure : options.secure,
     httpOnly: exists(newOptions.httpOnly) ? newOptions.httpOnly : options.httpOnly,
-    storeEncoded: exists(newOptions.storeEncoded) ? newOptions.storeEncoded : options.storeEncoded,
+    storeUnencoded: exists(newOptions.storeUnencoded) ? newOptions.storeUnencoded : options.storeUnencoded,
     sameSite: exists(newOptions.sameSite) ? newOptions.sameSite : options.sameSite
   };
+}
+
+export function safeDecodeURIComponent(str: string) {
+  try {
+    return decodeURIComponent(str);
+  } catch (e) {
+    return str;
+  }
+}
+
+export function safeJsonParse(str: string) {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return str;
+  }
 }
