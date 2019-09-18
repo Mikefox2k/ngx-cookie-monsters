30ffa51a3be0d8e3d8b254fafdf776d7131521fa
MI1490 <>
2019-09-18 15:23:11 +0200

    added model export to lib.ts

--- projects/ngx-cookie-monster/src/lib.ts
+++ projects/ngx-cookie-monster/src/lib.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-cookie-monster
  */
 
+export * from './lib/ngx-cookie-monster-options.model';
 export * from './lib/ngx-cookie-monster.service';
 export * from './lib/ngx-cookie-monster.module';
