1c8ccf56a397351a65cccc7022408bd8f3a05fff
MI1490 <>
2019-09-18 11:10:25 +0200

    added implements and members to service

--- projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.ts
+++ projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.ts
@@ -21,7 +21,7 @@ export interface ICookieOptions {
 }
 
 @Injectable()
-export class NgxCookieMonsterService {
+export class NgxCookieMonsterService implements ICookieOptions{
 
   protected options: CookieOptions;
 
@@ -36,4 +36,36 @@ export class NgxCookieMonsterService {
   constructor(private _provider: CookieProvider) {
     this.options = this._provider.cookieOptions;
   }
+
+  addTime(key: string, time: number | string | Date): void {
+  }
+
+  create(key: string, value: string, options?: CookieOptions): void {
+  }
+
+  delete(key: string, options?: CookieOptions): void {
+  }
+
+  deleteAll(options?: CookieOptions): void {
+  }
+
+  get(key: string): string {
+    return "";
+  }
+
+  getAll(): Object {
+    return undefined;
+  }
+
+  getObject(key: string): Object {
+    return undefined;
+  }
+
+  update(key: string, value: string, options?: CookieOptions): void {
+  }
+
+  updateOptions(key: string, options?: CookieOptions): void {
+  }
+
+
 }
