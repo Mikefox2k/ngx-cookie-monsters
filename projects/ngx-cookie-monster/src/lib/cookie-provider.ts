e77cf7c0f827d3826e55596462bcad2c7f1f223b
MI1490 <>
2019-09-18 10:42:28 +0200

    Create cookie-provider.ts

---
+++ projects/ngx-cookie-monster/src/lib/cookie-provider.ts
@@ -0,0 +1,29 @@
+import {CookieOptions} from './cookie-options.model';
+import {Inject, InjectionToken, Injector, Type} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {merge} from './ext';
+
+export const COOKIE_OPTIONS = new InjectionToken<CookieOptions>('COOKIE_OPTIONS');
+
+export class CookieProvider {
+
+  private defaultCookieOptions: CookieOptions;
+  private _cookieOptions: CookieOptions;
+
+  constructor(@Inject(COOKIE_OPTIONS) options: CookieOptions = {}, private _injector: Injector) {
+    this.defaultCookieOptions = {
+      path: this._injector.get<string>(APP_BASE_HREF + '/' as unknown as Type<string>),
+      domain: null,
+      expires: null,
+      secure: false,
+      httpOnly: false,
+      sameSite: 'none'
+    };
+
+    this._cookieOptions = merge(this.defaultCookieOptions, options);
+  }
+
+  get cookieOptions(): CookieOptions {
+    return this._cookieOptions;
+  }
+}
