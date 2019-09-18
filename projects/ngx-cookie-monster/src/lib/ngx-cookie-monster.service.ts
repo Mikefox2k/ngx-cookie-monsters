e30cb34c53e027c797d4753ac67757ccbfbb8701
MI1490 <>
2019-09-18 11:09:14 +0200

    added constructor and getter/setter for cookieService

--- projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.ts
+++ projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {CookieOptions} from './cookie-options.model';
+import {CookieProvider} from './cookie-provider';
 
 declare interface Document {
   cookie: string;
@@ -22,4 +23,17 @@ export interface ICookieOptions {
 @Injectable()
 export class NgxCookieMonsterService {
 
+  protected options: CookieOptions;
+
+  protected get cookieString(): string {
+    return document.cookie || '';
+  }
+
+  protected set cookieString(val: string) {
+    document.cookie = val;
+  }
+
+  constructor(private _provider: CookieProvider) {
+    this.options = this._provider.cookieOptions;
+  }
 }
