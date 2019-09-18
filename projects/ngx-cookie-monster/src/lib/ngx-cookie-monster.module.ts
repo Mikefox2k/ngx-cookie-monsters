e20e979dc011419f5c269d657ee03cd0521adf78
MI1490 <>
2019-09-18 12:38:40 +0200

    added providers for module

--- projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.module.ts
+++ projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.module.ts
@@ -1,10 +1,36 @@
-import {NgModule} from '@angular/core';
-
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CookieOptions } from './cookie-options.model';
+import {COOKIE_OPTIONS, CookieProvider} from './cookie-provider';
+import {NgxCookieMonsterService} from './ngx-cookie-monster.service';
+import {cookieFactory} from './cookie-factory';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: []
+  providers: [CookieProvider]
 })
-export class NgxCookieMonsterModule {
+export class CookieModule {
+  /**
+   * Use this method  to provide the CookieService
+   */
+  static forRoot(options: CookieOptions = {}): ModuleWithProviders {
+    return {
+      ngModule: CookieModule,
+      providers: [
+        {provide: COOKIE_OPTIONS, useValue: options},
+        {provide: NgxCookieMonsterService, useFactory: cookieFactory, deps: [CookieProvider]}
+      ]
+    };
+  }
+
+  /**
+   * Use this method (in non root modules) to import the directive/pipe
+   */
+  static forChild(options: CookieOptions = {}): ModuleWithProviders {
+    return {
+      ngModule: CookieModule,
+      providers: [
+        {provide: COOKIE_OPTIONS, useValue: options},
+        {provide: NgxCookieMonsterService, useFactory: cookieFactory, deps: [CookieProvider]}
+      ]
+    };
+  }
 }
