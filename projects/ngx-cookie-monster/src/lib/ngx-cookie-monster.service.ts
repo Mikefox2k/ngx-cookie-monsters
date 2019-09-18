a372006f35a6961a44b0f59647f85ea55011748f
MI1490 <>
2019-09-18 11:06:59 +0200

    updated cookieservice
    - added interface Document
    - added const document
    - added interface ICookieOptions
    - removed providedIn in service

--- projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.ts
+++ projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CookieOptions} from './cookie-options.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+declare interface Document {
+  cookie: string;
+}
+
+declare const document: Document;
+
+export interface ICookieOptions {
+  get(key: string): string;
+  getObject(key: string): Object;
+  getAll(): Object;
+  create(key: string, value: string, options?: CookieOptions): void;
+  update(key: string, value: string, options?: CookieOptions): void;
+  updateOptions(key: string, options?: CookieOptions): void;
+  delete(key: string, options?: CookieOptions): void;
+  deleteAll(options?: CookieOptions): void;
+  addTime(key: string, time: number | string | Date): void;
+}
+
+@Injectable()
 export class NgxCookieMonsterService {
 
-  constructor() { }
 }
