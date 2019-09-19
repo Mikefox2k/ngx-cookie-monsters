2ebb84250262ea6d85579503709f85979e1858ea
MI1490 <>
2019-09-19 13:18:01 +0200

    added new test

--- projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.spec.ts
+++ projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.spec.ts
@@ -1,9 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
 
 import { NgxCookieMonsterService } from './ngx-cookie-monster.service';
+import {CookieModule} from "./ngx-cookie-monster.module";
+import {Injector} from "@angular/core";
 
 describe('NgxCookieMonsterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let injector: Injector;
+  let cookieService: NgxCookieMonsterService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CookieModule.forRoot()
+      ]
+    });
+    injector = getTestBed();
+    cookieService = injector.get(NgxCookieMonsterService);
+  });
 
   it('should be created', () => {
     const service: NgxCookieMonsterService = TestBed.get(NgxCookieMonsterService);
