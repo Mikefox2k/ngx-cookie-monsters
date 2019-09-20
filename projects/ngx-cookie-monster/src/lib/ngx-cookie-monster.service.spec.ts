44410d784e83ed877c8804958e3a8f8c55d3f72e
MI1490 <>
2019-09-20 11:40:58 +0200

    created test cases for successful create and get

--- projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.spec.ts
+++ projects/ngx-cookie-monster/src/lib/ngx-cookie-monster.service.spec.ts
@@ -1,36 +1,28 @@
-import {getTestBed, TestBed} from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import {NgxCookieMonsterService} from './ngx-cookie-monster.service';
 import {CookieModule} from "./ngx-cookie-monster.module";
-import {Injector, Type} from "@angular/core";
-import {APP_BASE_HREF} from "@angular/common";
-import {NgxCookieMonsterProvider} from "./ngx-cookie-monster-provider";
-import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
-import {BrowserTestingModule} from "@angular/platform-browser/testing";
 
 describe('NgxCookieMonsterService', () => {
-
-  let injector: Injector;
-  let cookieService: NgxCookieMonsterService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         CookieModule.forRoot(),
-        BrowserDynamicTestingModule,
-        BrowserTestingModule
-      ],
-      providers: [
-        { provide: APP_BASE_HREF, useClass: NgxCookieMonsterProvider},
-        NgxCookieMonsterService
       ]
     });
-    cookieService = TestBed.get(NgxCookieMonsterService as Type<NgxCookieMonsterService>);
   });
 
-  it('should be created', () => {
-    expect(NgxCookieMonsterService).toBeDefined(); // undefined weil kein InjectionToken
-    expect(cookieService).toBeDefined(); // geht durch
-    expect(cookieService instanceof NgxCookieMonsterService).toBeTruthy(); // NgxCookieMonsterService ist undefined
-  });
+  it('should be created', inject([NgxCookieMonsterService], (cookieService: NgxCookieMonsterService) => {
+    expect(NgxCookieMonsterService).toBeDefined();
+    expect(cookieService).toBeDefined();
+    expect(cookieService instanceof NgxCookieMonsterService).toBeTruthy();
+  }));
+
+  it('should create and get cookie', inject([NgxCookieMonsterService], (cookieService: NgxCookieMonsterService) => {
+    const key = 'test';
+    const value = 'valu';
+    cookieService.create(key, value);
+    expect(cookieService.get(key)).toBe(value);
+  }));
 });
